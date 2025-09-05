bcccbfb7502de27fdeb3f250d0f4215d391ab868
Maximilian <scientificresearch309@gmail.com>
2025-09-05 10:34:58 +0200

    The form and CSS file is there

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,25 @@ function App() {
   return (
     <div className="App">
       <h1>Form-frontend-backend-with-json-server-Reatc-Vite-Typescript</h1>
+
+      <form>
+        <fieldset>
+          <legend>New Job</legend>
+          <div className="row">
+            <label>Job Title</label>
+            <div>
+              <input type="text" />
+            </div>
+          </div>
+
+          <div className="row">
+            <label>Description</label>
+            <div>
+              <textarea />
+            </div>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 }
