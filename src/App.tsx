3232ad8511ee992ec71f535b22f1d3daa5e588af
Maximilian <scientificresearch309@gmail.com>
2025-09-05 11:20:34 +0200

    a simple initial data is there

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
 // import './App.css';
 
+import { useState } from 'react';
+
+const _formData = {
+  jobTitle: '',
+  description: '',
+};
+
 function App() {
+  const [formData, setFormData] = useState(_formData);
+
   return (
     <div className="App">
       <h1>Form-frontend-backend-with-json-server-Reatc-Vite-Typescript</h1>
 
-      <form>
-        <fieldset>
-          <legend>New Job</legend>
-          <div className="row">
-            <label>Job Title</label>
-            <div>
-              <input type="text" />
+      <section>
+        <form>
+          <fieldset>
+            <legend>New Job</legend>
+            <div className="row">
+              <label>Job Title</label>
+              <div>
+                <input type="text" />
+              </div>
             </div>
-          </div>
 
-          <div className="row">
-            <label>Description</label>
-            <div>
-              <textarea />
+            <div className="row">
+              <label>Description</label>
+              <div>
+                <textarea />
+              </div>
             </div>
-          </div>
-        </fieldset>
-      </form>
+          </fieldset>
+        </form>
+
+        <div className="debuggingArea">
+          <pre>{JSON.stringify(formData)}</pre>
+        </div>
+      </section>
     </div>
   );
 }
